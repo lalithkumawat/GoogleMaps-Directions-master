c87922a3954d677fd06350938c55fb799c6668bd
Aman Kalaskar <lkkumawat123@gmail.com>
2019-04-04 12:05:35 +0530

    Running

---
+++ app/src/main/java/com/example/priyanka/GetDirectionsData.java
@@ -0,0 +1,92 @@
+package com.example.priyanka;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+
+import java.io.IOException;
+import java.nio.DoubleBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class GetDirectionsData extends AsyncTask<Object,String,String> {
+
+    GoogleMap mMap;
+    String url;
+    String googleDirectionsData;
+    String duration, distance;
+    LatLng latLng;
+
+    ArrayList dur = new ArrayList();
+    ArrayList al = new ArrayList();
+    DataParser parser;
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        mMap = (GoogleMap)objects[0];
+        url = (String)objects[1];
+        latLng = (LatLng)objects[2];
+
+
+
+        DownloadUrl downloadUrl = new DownloadUrl();
+        try {
+            googleDirectionsData = downloadUrl.readUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googleDirectionsData;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+        String[] directionsList;
+        parser = new DataParser();
+        dur.add(parser.duration);
+        directionsList = parser.parseDirections(s);
+        al.add(directionsList);
+//        displayDirection(directionsList);
+
+    }
+
+    public void createPath(int in)
+    {
+        String[] directionList = Arrays.asList(al.get(in)).toArray(new String[0]);
+        displayDirection(directionList);
+    }
+
+    public void displayDirection(String[] directionsList)
+    {
+
+        int count = directionsList.length;
+        for(int i = 0;i<count;i++)
+        {
+            PolylineOptions options = new PolylineOptions();
+            options.color(Color.RED);
+            options.width(10);
+            options.addAll(PolyUtil.decode(directionsList[i]));
+
+            mMap.addPolyline(options);
+        }
+    }
+
+
+
+
+
+
+}
+
